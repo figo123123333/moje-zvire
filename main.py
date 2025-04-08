d84d96fdc04d5303d997f7d7e2e222dfd5e548fc
figo123123333 <filip.lukas@infis.cz>
2025-04-08 09:34:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+oblibeneZvire = input("jaké ja vaše oblíbené zvíře")
+print("vaše oblíbené zvíře je",oblibeneZvire)
+
 zvire = input("jak se jmenuje tvoje zvíře?")
 print("tvoje zvíře se jemnuje",zvire)
